51b82ffe19817e3c5405a68749b914522db9abaf
Samuel Waugh <swaugh@rodeofx.com>
2019-07-22 13:57:51 -0400

    first setup of application entrypoint

--- AIGIS.py
+++ AIGIS.py
@@ -1,3 +1,20 @@
 """
 Application entrypoint.
 """
+from argparse import ArgumentParser
+from core.AigisCore import Aigis  #pylint: disable=no-name-in-module
+
+
+def main():
+    """
+    Boot up Aigis core.
+    """
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--config', dest='cpath', help='Configuration file path', required=True)
+    options = parser.parse_args()
+
+    Aigis(options.cpath)
+
+
+if __name__ == "__main__":
+    main()
