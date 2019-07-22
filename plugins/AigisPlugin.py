70f0002b4cfec18ac85c9bf288273474814df962
Samuel Waugh <swaugh@rodeofx.com>
2019-07-22 13:54:59 -0400

    Started AigisPlugin

--- plugins/AigisPlugin.py
+++ plugins/AigisPlugin.py
@@ -1,3 +1,27 @@
 """
 Representation of a plugin with handlers used by the core to properly route traffic.
 """
+
+
+class AigisPlugin():
+    """
+    A plugin that can be managed by AIGIS and it's core.
+    """
+    def __init__(self):
+        self.id = id(self)
+
+    def __eq__(self, other):
+        """
+        Determine if two plugins are the same based on their attributed UID.
+        This only works between two AigisPlugin objects. Otherwise will return false.
+
+        :param AigisPlugin other: other plugin against which to check
+
+        :returns: boolead equivalence
+        :rtype: bool
+        """
+        try:
+            return self.id == other.id
+        except AttributeError:
+            pass
+        return False
