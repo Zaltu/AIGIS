02e84f18ac6bb40cf0932cb124647364d32585d2
Samuel Waugh <swaugh@rodeofx.com>
2019-07-22 13:55:30 -0400

    basic instantiation for AigisCore

--- core/AigisCore.py
+++ core/AigisCore.py
@@ -1,3 +1,24 @@
 """
 Responsible for the maintenance and redirecting of all AIGIS's core and plugin features
 """
+from plugins.PluginManager import PluginManager
+
+class Aigis():
+    """
+    Did I succeed in protecting everyone?
+
+    :param str config: file path to the config to be used for this Aigis instance.
+    """
+    plugins = PluginManager()
+    def __init__(self, config):
+        pass
+
+
+
+
+
+# Aigis needs to
+# - load the config
+# - prep and start the log service
+# - call the plugin loader
+# - ???
