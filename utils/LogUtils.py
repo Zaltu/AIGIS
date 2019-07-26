bace1e4c063da7b6e182abf2d1e6d8bac729b9ee
Samuel Waugh <swaugh@rodeofx.com>
2019-07-26 15:55:39 -0400

    utility functions and setup for logging

---
+++ utils/LogUtils.py
@@ -0,0 +1,42 @@
+"""
+Container module for logging utility functions.
+"""
+import os
+import sys
+import logging
+
+PLUGIN_LOG_LOCATION = os.path.join(os.path.dirname(__file__), "../log/plugins/")
+CORE_LOG_LOCATION = os.path.join(os.path.dirname(__file__), "../log/{corename}.log")
+
+_SH = logging.StreamHandler(sys.stdout)
+_SH.setFormatter(logging.Formatter('%(asctime)s : %(levelname)s : %(message)s'))
+
+if not os.path.exists(os.path.abspath(os.path.join(os.path.dirname(__file__), "../log"))):
+    os.mkdir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../log")))
+
+def _plugin_file_name(plugin):
+    """
+    Fetch the filename of a plugin's log file.
+
+    :param AigisPlugin plugin: an AigisPlugin
+
+    :returns: absolute log file path
+    :rtype: str
+    """
+    return os.path.abspath(os.path.join(PLUGIN_LOG_LOCATION, "_".join([plugin.name, plugin.id])))
+
+def _add_log_handlers(logger, logfile):
+    """
+    Add the file and stream handlers to a logger.
+
+    :param logging.logger logger: the logger to configure
+    :param str logfile: file to log to disk
+    """
+    if not os.path.exists(logfile):
+        with open(logfile, 'w+'):
+            pass
+    logger.setLevel(logging.INFO)
+    fh = logging.FileHandler(logfile)
+    fh.setFormatter(logging.Formatter('%(asctime)s : %(levelname)s : %(message)s'))
+    logger.addHandler(fh)
+    logger.addHandler(_SH)
