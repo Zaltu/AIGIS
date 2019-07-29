45ae26b0e9e51dd224f15347bcb9d0d669021112
Samuel Waugh <swaugh@rodeofx.com>
2019-07-29 12:23:46 -0400

    prevent process from dying and smoothly handle system interrupt

--- AIGIS.py
+++ AIGIS.py
@@ -1,9 +1,12 @@
 """
 Application entrypoint.
 """
+import signal
 from argparse import ArgumentParser
+from threading import Event
 from core.AigisCore import Aigis  #pylint: disable=no-name-in-module
 
+SHUTDOWN_EVENT = Event()
 
 def main():
     """
@@ -13,7 +16,14 @@ def main():
     parser.add_argument('-c', '--config', dest='cpath', help='Configuration file path', required=True)
     options = parser.parse_args()
 
+    # Insure any interrupt is handled smoothly.
+    signal.signal(signal.SIGINT, lambda a, b: SHUTDOWN_EVENT.set())
+
+    # Launch Aigis
     Aigis(options.cpath)
+
+    # Wait for shutdown event
+    SHUTDOWN_EVENT.wait()
 
 
 if __name__ == "__main__":
