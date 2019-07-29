285033ad0052a848d18f8f4bea4ede572da7fa78
Samuel Waugh <swaugh@rodeofx.com>
2019-07-29 10:33:44 -0400

    added custom logging levels

--- utils/LogUtils.py
+++ utils/LogUtils.py
@@ -5,14 +5,39 @@ import os
 import sys
 import logging
 
+
+### Define extra custom logging levels.
+### Mostly used for fluff, but potentially usefull also for log tracking and management.
+### We define BOOT and SHUTDOWN here.
+BOOTUP = logging.ERROR + 1
+logging.addLevelName(BOOTUP, "BOOT")
+def boot(self, message, *args, **kws):  #pylint: disable=missing-docstring
+    if self.isEnabledFor(BOOTUP):
+        # Yes, logger takes its '*args' as 'args'.
+        self._log(BOOTUP, message, args, **kws)
+logging.Logger.boot = boot
+
+
+SHUTDOWN = logging.ERROR + 2
+logging.addLevelName(SHUTDOWN, "SHUTDOWN")
+def shutdown(self, message, *args, **kws):  #pylint: disable=missing-docstring
+    if self.isEnabledFor(SHUTDOWN):
+        # Yes, logger takes its '*args' as 'args'.
+        self._log(SHUTDOWN, message, args, **kws)
+logging.Logger.shutdown = shutdown
+
+
 PLUGIN_LOG_LOCATION = os.path.join(os.path.dirname(__file__), "../log/plugins/")
 CORE_LOG_LOCATION = os.path.join(os.path.dirname(__file__), "../log/{corename}.log")
+
 
 _SH = logging.StreamHandler(sys.stdout)
 _SH.setFormatter(logging.Formatter('%(asctime)s : %(levelname)s : %(message)s'))
 
+
 if not os.path.exists(os.path.abspath(os.path.join(os.path.dirname(__file__), "../log"))):
     os.mkdir(os.path.abspath(os.path.join(os.path.dirname(__file__), "../log")))
+
 
 def _plugin_file_name(plugin):
     """
