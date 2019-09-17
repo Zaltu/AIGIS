ed3337793b75294c764be443c4acd9eb109feeba
Samuel Waugh <swaugh@rodeofx.com>
2019-09-17 11:50:54 -0400

    commented exc_utils module

--- utils/exc_utils.py
+++ utils/exc_utils.py
@@ -1,3 +1,8 @@
+"""
+Container module for "abstract" exception classes so they can be easily depended on by any module.
+This was I never need to worry about interdependancies caused by silly exception class proxies.
+"""
+
 class PluginLoadError(Exception):
     """
     Parent class for plugin load exceptions.
