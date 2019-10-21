df8b355cd4556de5bfb20fb3af3cb7aef06c8499
Samuel Waugh <swaugh@rodeofx.com>
2019-10-21 13:58:45 -0400

    indicate default config to load

--- AIGIS.py
+++ AIGIS.py
@@ -1,19 +1,21 @@
 """
 Application entrypoint.
 """
+import os
 import signal
 from argparse import ArgumentParser
 from threading import Event
 from core.AigisCore import Aigis  #pylint: disable=no-name-in-module
 
 SHUTDOWN_EVENT = Event()
+DEFAULT_CONFIG = os.path.abspath(os.path.join(os.path.dirname(__file__), "config", "config.aigis"))
 
 def main():
     """
     Boot up Aigis core.
     """
     parser = ArgumentParser()
-    parser.add_argument('-c', '--config', dest='cpath', help='Configuration file path', required=True)
+    parser.add_argument('-c', '--config', dest='cpath', help='Config file path', default=DEFAULT_CONFIG)
     options = parser.parse_args()
 
     # Insure any interrupt is handled smoothly.
