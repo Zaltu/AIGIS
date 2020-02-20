af8fcabb2c7579ccfc55a806a59d304acdd73b4b
Zaltu <swwouf@hotmail.com>
2020-02-20 12:41:45 -0500

    added AIGISReload function

--- plugins/core/Skills.py
+++ plugins/core/Skills.py
@@ -1,6 +1,7 @@
 """
 Container class for the singleton which holds all core plugins' modules for shared use.
 """
+#pylint: disable=invalid-name
 from utils import exc_utils  #pylint: disable=no-name-in-module
 
 
@@ -9,7 +10,26 @@ class Skills():
     The skills class is built to mock dynamic runtime inheritance based on loaded plugins.
     Only a single instance of this class should be made, and the private functions it exposes
     (_AIGISlearnskill and _AIGISrecurdict) should only be called by the core AIGIS code.
+
+    AIGISReload is intentionally exposed to allow plugins to request others to reload themselves.
+
+    :param PluginManager manager: the plugin manager singleton
     """
+    def __init__(self, manager):
+        self.__plugin_manager__ = manager
+
+    def AIGISReload(self, plugin_name):
+        """
+        Request AIGIS to reload a certain plugin.
+        If a plugin tries to self-reload, it might have a bad time.
+
+        :param str plugin_name: name of plugin to reload
+        """
+        for plugin in self.__plugin_manager__:
+            if plugin.name == plugin_name:
+                plugin.loader.reload(plugin, self.__plugin_manager__)
+                break
+
     def _AIGISlearnskill(self, mod, plugin):
         """
         Join a given dict with this class' dict, essentially extending the functionality of the class.
